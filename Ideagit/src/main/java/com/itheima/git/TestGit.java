71549bb29a8047234b5b88fa3df9df7d49fc8164
fenghun <592791420@qq.com>
2018-11-10 17:09:13 +0800

    first commit

---
+++ Ideagit/src/main/java/com/itheima/git/TestGit.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("!21");
+        System.out.println("!21");
+        System.out.println("!21");
+        System.out.println("!21");
+        System.out.println("!21");
+    }
+}
